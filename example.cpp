9c67afc1c96cedebc0b7b1590e64c77752e6d837
Sandu Bogdan <cornusandu@hotmail.com>
2026-01-15 12:39:38 +0200

    Fixed

--- example.cpp
+++ example.cpp
@@ -4,12 +4,11 @@
 int main() {
     windowsmod_init();
 
-    std::shared_ptr<void*> h0 = createFile("test.txt", enum_::fs::FileCreatePerms::FILE_READ, enum_::fs::FileAttributes::READONLY_FILE);
+    std::shared_ptr<void> h0 = createFile("test.txt", enum_::fs::FileCreatePerms::FILE_READ, enum_::fs::FileAttributes::READONLY_FILE);
 
-    std::shared_ptr<void*>
-    h1 = createFile(0x0, enum_::fs::FileCreatePerms::FILE_READ, enum_::fs::FileAttributes::READONLY_FILE);
+    std::shared_ptr<void> h1 = createFile(0x0, enum_::fs::FileCreatePerms::FILE_READ, enum_::fs::FileAttributes::READONLY_FILE);
 
-    if (!*h1.get()) {
+    if (!h1.get()) {
         printf(getLastError());
     }
 
